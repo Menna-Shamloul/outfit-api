eb77afb8457f5cbc1a2cf8201c0b678bdc33214e
Menna Shamloul <menna.shamloul92@gmail.com>
2024-03-03 07:44:41 +0000

    add followes app and serilizers and views

---
+++ followers/views.py
@@ -0,0 +1,30 @@
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
+
+
+class FollowerList(generics.ListCreateAPIView):
+    """
+    List all followers,
+    following another user'.
+    Create a follower,
+    Perform_create: associate the current logged in user with a follower.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a follower
+    No Update view,
+    Destroy a follower,
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
