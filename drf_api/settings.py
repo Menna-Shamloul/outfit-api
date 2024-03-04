3387ee496b50ef87c7b163ac40de4a229596c994
Menna Shamloul <menna.shamloul92@gmail.com>
2024-03-04 19:35:06 +0000

    readd jsonrender

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -38,7 +38,7 @@ REST_FRAMEWORK = {
 }
 if 'DEV' not in os.environ:
     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
-        'rest_framework.renderers.JSONRender',
+        'rest_framework.renderers.JSONRenderer',
     ]
 
 REST_USE_JWT = True
